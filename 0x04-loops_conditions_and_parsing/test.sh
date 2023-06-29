fb67cd4200706b59b6de4c5684e77459a5059220
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-06-29 21:51:19 +0300

    Pass school as variable

---
+++ 0x04-loops_conditions_and_parsing/test.sh
@@ -0,0 +1,10 @@
+file school
+./9-to_file_or_not_to_file
+touch school
+./9-to_file_or_not_to_file
+echo 'betty' > school
+./9-to_file_or_not_to_file
+rm school
+mkdir school
+./9-to_file_or_not_to_file 
+rmdir school
