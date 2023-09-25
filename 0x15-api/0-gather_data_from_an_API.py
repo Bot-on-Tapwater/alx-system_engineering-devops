de3946eea2c41e029b111d49230a4bc07db6297e
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-25 09:38:11 +0300

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""Gather data from an API"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+    import urllib
+
+    # grab id from cmd args
+    employee_id = sys.argv[1]
+
+    # api url
+    user_api_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    todos_api_url = f'https://jsonplaceholder.typicode.com/todos'
+    params = {'userId': employee_id}
+
+    # use requests to fetch data from url
+    employee_details = requests.get(user_api_url)
+
+    emp_name = employee_details.json()['name']
+
+    total_todos = requests.get(todos_api_url, params)
+
+    cmpl_tds = []
+
+    for todo in total_todos.json():
+        if todo['completed'] is True:
+            cmpl_tds.append(todo)
+
+    task_completion_ratio = f'({len(cmpl_tds)}/{len(total_todos.json())})'
+
+    print(f'Employee {emp_name} is done with tasks{task_completion_ratio}:')
+
+    for todo in cmpl_tds:
+        print(f'\t{todo["title"]}')
