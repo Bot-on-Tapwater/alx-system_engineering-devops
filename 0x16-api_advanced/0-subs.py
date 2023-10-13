e8d913d1475afd0784aaa3dc361a028ec8344aea
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-10-13 09:20:14 +0300

    Refactor code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python3
-
-"""Query number of subscribers for a subreddit"""
-
+'''Returns number of subscibers in a subreddit'''
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Query number of subscribers in subreddit"""
-
-    # url link to the reddit api
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
-    # send a GET request
-    response = requests.get(url)
-
+    '''If not a valid subreddit, return 0'''
+    url = ('https://www.reddit.com/r/{}/about.json'.format(subreddit))
+    response = requests.get(url, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-
-        subscribers = data["data"]["subscribers"]
-        # print(subscribers)
-        return (subscribers)
-
+        return (data['data']['subscribers'])
     else:
-        # print(f"Error: \
-        #       Status code {response.status_code}.\
-        #           Response text: {response.text}")
-        return (0)
+        return 0
