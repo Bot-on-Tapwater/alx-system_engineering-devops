3c1eae1d01a6d963da71aebc54909b23a987db6a
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-10-13 11:17:23 +0300

    Refactor Raymond

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,41 +1,34 @@
 #!/usr/bin/python3
-
-"""Get 10 hot posts for a subreddit recursively"""
+"""
+A recursive function that queries the Reddit API and
+returns a list containing the titles of all hot articles
+for a given subreddit. If no results are found for the
+given subreddit, the function should return None.
+"""
 
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=""):
-    """Get 10 hot posts for a subreddit recursively"""
-
-    # url link to the reddit api
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-
-    # prevents 429 status code (Rate limit)
-    headers = {"User-Agent": "Custom"}
-
-    # retrieve only 10 results
-    params = {"after": after}
-
-    # send a GET request
-    response = requests.get(url, headers=headers,
-                            params=params, allow_redirects=False)
+    """
+    func to query an api recursively
+    """
+    response = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"after": after},
+    )
 
     if response.status_code == 200:
-        # get http response and convert from json type
-        data = response.json()
-
-        hotposts = data['data']['children']
-
-        for child in hotposts:
-            hot_list.append(child['data']['title'])
-
-        after = data['data']['after']
+        for get_data in response.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            hot_list.append(title)
+        after = response.json().get("data").get("after")
 
         if after is None:
             return hot_list
         else:
             return recurse(subreddit, hot_list, after)
-
     else:
-        return (None)
+        return None
